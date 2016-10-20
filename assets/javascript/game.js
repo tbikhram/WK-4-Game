fed565f3896cce446af39e0f0ee78c69bfc4fe0b
trevor bikhram <tbikhram@trevors-MacBook-Pro.local>
2016-10-20 08:25:56 -0700

    this is the updated version 1 js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,98 @@
 $ (document).ready(function(){
 // create the variables of the game, this is what the functions will do
 var randomNumber = 0;
-var wins= 0;
-var losses=0;
-var counter= 0
+var Win= 0;
+var Loss=0;
+var sumPoints=0;
 var randomCrystal= [1,5,15,13];
 var totalNumber=0;
-var randomNumber1=(Math.floor(Math.random() *120 )+19)
+//var randomNumber1=(Math.floor(Math.random() *120 )+19)
 
 
-var diaMonds1=0;
-var diaMonds2;
-var diaMonds3;
-var diaMonds4;
+//var diamonds1=0;
+//var diamonds2;
+//var diamonds3;
+//var diamonds4;
 
 
-// this is the first function to get the random numbers 
+// this is the first function to get the random number
 function getRandomInt(min, max) {
       min = Math.ceil(min);
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+// this is place a random number between 19 and 121
+function placeRandom(){
+		randomNumber = getRandomInt(19,121);
+		$("#randomNumber").html(randomNumber);
+}
 
-  randomNumber= getRandomInt(19,120)
-  $(".showNumber").html(randomNumber1);
-  console.log(randomNumber);
+  //randomNumber= getRandomInt(19,120)
+ // $(".showNumber").html(randomNumber1);
+ // console.log(randomNumber);
 //assinging random variables
-  for(var i=0; i<randomCrystal.length; i++){
-  	randomCrystal[i]=getRandomInt(1,12);
-  }
+ // for(var i=0; i<randomCrystal.length; i++){
+ // 	randomCrystal[i]=getRandomInt(1,12);
+ // }
+
+// here we asign the crystals and using the array to get values for each crystal
+ function assigncrystalVal(){
+ 	for(var i=0; i <randomCrystal.length; i++){
+		randomCrystal[i] = getRandomInt(1,20);
+ 	}
+ 	$("#crystal1").attr("value", randomCrystal[0]);	
+ 	$("#crystal2").attr("value", randomCrystal[1]);	
+ 	$("#crystal3").attr("value", randomCrystal[2]);	
+ 	$("#crystal4").attr("value", randomCrystal[3]);	
+
+ }
+	 placeRandom();
+	 assigncrystalVal();
+
+	 $("#randomNumber").html(randomNumber);
+	 $("gemScores").html(sumPoints);
+	 $("#Win").html(Win);
+	 $("#Loss").html(loss);
+
+
+
+	// statements to make click true and false so when clicked it has a value that is true
+	 $(".crystal").click(function){
+	 	if(sumPoints<randomNumber)
+	 }
 
 
 
 
 
-  function testScore(){
-  	console.log("test1");
-  	if(counter== randomNumber1){
-  		alert("you won");
-  		win++;
-  		$("#win").text("wins:"+ win);
-  		newgame();
-  	}else if (counter > randomNumber1){
-  		alert("you lose");
-  		lose++;
-  		$("#lose").text("lose" + lose);
-  		newGame()
-  	}
+
+//	function testScore(){
+  //	console.log("test1");
+  	//if(counter== randomNumber1){
+  	//	alert("you won");
+  	//	win++;
+  	//	$("#win").text("wins:"+ win);
+  	//	newgame();
+  	//}else if (counter > randomNumber1){
+  	//	alert("you lose");
+  	//	lose++;
+  	//	$("#lose").text("lose" + lose);
+  	//	newGame()
+  	//}
   	
   	
-  };
+  //};
  
  //this is the step that asigns a random crystal number to a crystal image
 
- $(".diaMonds1").on('click', function(){
- 	if(diaMonds1 ===0 ){
-	diaMonds1 = randomCrystal[Math.floor(Math.random() * randomCrystal.length)];
-			$(".diaMonds1").val(diaMonds1);}
-			counter =counter + (diaMonds1);
-			$("#yourScore").text("Your Score"+ counter);
- 	console.log(diaMonds1);
- });
+ //$(".diamond1").on('click', function(){
+ //	if(diamond1 ===0 ){
+//	diamond1 = randomCrystal[Math.floor(Math.random() * randomCrystal.length)];
+//			$(".diamond1").val(diamond1);}
+//			counter =counter + (diamond1);
+//			$("#yourScore").text("Your Score"+ counter);
+ //	console.log(diamond1);
+ //});
 
  
 
